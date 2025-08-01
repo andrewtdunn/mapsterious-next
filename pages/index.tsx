bea339156c3949d4ba348f0be23b64065ad5440f
Andrew Dunn <atd2005@gmail.com>
2025-07-31 21:28:46 -0500

    added per user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const deleteTodo = (id: string) => {
 };
 
 export default function App() {
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
